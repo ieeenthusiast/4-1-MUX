b6efb57968872d70de4c7151b917a7684eb76ff0
ieeenthusiast <46030610+ieeenthusiast@users.noreply.github.com>
2019-04-11 11:56:19 +0200

    Update MUX4_1.vhd

--- MUX4_1.vhd
+++ MUX4_1.vhd
@@ -1,9 +1,9 @@
 ------------------------------------------------------
 -- Project		:	4:1 Multiplexer
--- Description	:	--
+-- Description		:	--
 -- Author		:	Lars Mannshardt
 -- Created		:	11.04.2019
--- Last Update	:	11.04.2019
+-- Last Update		:	11.04.2019
 ------------------------------------------------------
 
 library IEEE;
@@ -12,8 +12,8 @@ use ieee.numeric_std.all;
 
 entity MUX4_1 is port (
 	I0,I1,I2,I3	: 	in 	std_logic;	--inputs
-	S				:	in		std_logic_vector(1 downto 0);	--select
-	Q				:	out	std_logic);	--output
+	S		:	in		std_logic_vector(1 downto 0);	--select
+	Q		:	out	std_logic);	--output
 end MUX4_1;
 
 architecture MUX4_ARCH of MUX4_1 is
